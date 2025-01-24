d5c0727e6b33200f0c41b2be2c083ca69a107ce1
admin <krushnak915@gmail.com>
2025-01-24 16:01:27 +0530

    find even

--- src/com/java8/streamapi.java
+++ src/com/java8/streamapi.java
@@ -1,9 +1,16 @@
 package com.java8;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class streamapi
 {
     public static void main(String[] args) {
+
         System.out.println("java 8");
+
+        List<Integer> li= Arrays.asList(1,3,5,76,5,4,3,22);
+        li.stream().filter(e->e%2!=0).forEach(System.out::println);
     }
 
 }

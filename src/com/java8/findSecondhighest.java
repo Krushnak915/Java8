f3d181b61c9793be45a91a6fbeceaf4ae3a244ab
admin <krushnak915@gmail.com>
2025-01-31 14:52:19 +0530

    added comment

--- src/com/java8/findSecondhighest.java
+++ src/com/java8/findSecondhighest.java
@@ -9,5 +9,7 @@ public class findSecondhighest {
         List<Integer> li = Arrays.asList(12,34,56,67,43,23,22);
         Integer i = li.stream().sorted(Comparator.reverseOrder()).skip(1).findFirst().get();
         System.out.println(i);
+
+        //add comment
     }
 }
